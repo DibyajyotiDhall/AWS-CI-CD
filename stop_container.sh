f62547793aad82851e515859f0ae61e35f5531d4
DibyajyotiDhall <71689153+DibyajyotiDhall@users.noreply.github.com>
2023-11-02 13:17:00 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# this helps if any container running in that same port you are deploying is up to be stopped so that new image created that shoould be uploaded.
+# this helps if any container running in that same port you are deploying is up to be stopped so that new image created that shoould be uploaded.t
 
 # Stop the running container (if any)
 docker stop $(docker ps -a -q)
